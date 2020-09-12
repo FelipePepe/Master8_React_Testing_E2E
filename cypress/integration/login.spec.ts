3ba135836b24b60b9814a9a3ada8db352135222f
Felipe Pepe <FelipePepe@gmail.com>
2020-09-12 11:41:42 +0200

    first commit

---
+++ cypress/integration/login.spec.ts
@@ -0,0 +1,86 @@
+describe('Login specs', () => {
+  it('visit the login page', () => {
+    cy.visit('/');
+  });
+
+  it('should name input has the focus when it clicks on it', () => {
+    // Arrange
+
+    // Act
+    cy.visit('/');
+    cy.findByRole('textbox').click();
+
+    //cy.get('input[name="name"]').click();
+
+    // Assert
+    //cy.get('input[name="name"]').should('have.focus');
+    cy.findByRole('textbox').should('have.focus');
+  });
+
+  it('should show an alert with a message when type invalid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = '1234';
+
+    // Act
+    cy.visit('/');
+    cy.get('input[name="name"]').as('userInput');
+    cy.get('input[name="password"]').as('passwordInput');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+  });
+
+  it('should show an alert with a message when type invalid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = '1234';
+
+    cy.on('window:alert', cy.stub().as('alertStub'));
+
+    // Act
+    cy.visit('/');
+    cy.findByLabelText('Name').as('userInput');
+    cy.findByLabelText('Password').as('passwordInput');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+
+    cy.findByRole('button', { name: 'Login' }).click();
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+
+    cy.get('@alertStub').should('have.been.calledWidth', 'invalid credentials, use admin/test, excercise: display a mui snackbar instead of this alert.');
+
+
+  });
+
+  it('should show an alert with a message when type invalid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = 'test';
+
+    // Act
+    cy.visit('/');
+    cy.findByLabelText('Name').as('userInput');
+    cy.findByLabelText('Password').as('passwordInput');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+
+    cy.findByRole('button', { name: 'Login' }).click();
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+
+    cy.url().should('equal', 'http://localhost:8080/#/hotel-collection');
+
+  });
+});
